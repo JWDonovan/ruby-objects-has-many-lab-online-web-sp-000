3a70001973d69b0e971e0b1a2cb1f2410bdb058d
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 21:37:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,16 @@
 class Artist
   attr_accessor :name, :songs
 
-  def initialize(name)
+  def initialize(name, songs)
     @name = name
-    @songs = []
+    @songs = songs
   end
 
   def add_song(song)
     @songs << song
+  end
+
+  def add_song_by_name(song_name)
+    
   end
 end
